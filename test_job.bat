eb1be52361ba8c47b8596ace03ced574c023d2e8
yasuhiko-nishio <yasuhiko-nishio@mamezou.com>
2022-05-25 16:59:42 +0900

    テストコード実行文追加

--- test_job.bat
+++ test_job.bat
@@ -6,4 +6,4 @@ docker-compose ps
 
 docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ls -al"
 
-docker exec docker_mysql_db_1 bash -c "sudo chmod +x /var/dummy/sample_test.sh && ls -al"
+docker exec docker_mysql_db_1 bash -c "./var/dummy/sample_test.sh"
