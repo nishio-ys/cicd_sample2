db41b855de96a0f292318f59ad3051dec4ce3590
yasuhiko-nishio <yasuhiko-nishio@mamezou.com>
2022-05-25 16:38:52 +0900

    マウント先の表示

--- test_job.bat
+++ test_job.bat
@@ -4,6 +4,6 @@ docker-compose up -d
 
 docker-compose ps
 
-docker exec docker_mysql_db_1 bash -c "cd /var && ls -al"
+docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ls -al"
 
 docker exec docker_mysql_db_1 bash -c "pwd"
