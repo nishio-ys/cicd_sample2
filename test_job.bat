6c38ae38a26bbfc83a0f592232e0c08f4b728c89
yasuhiko-nishio <yasuhiko-nishio@mamezou.com>
2022-05-25 16:43:34 +0900

    実行権限の付与

--- test_job.bat
+++ test_job.bat
@@ -6,4 +6,4 @@ docker-compose ps
 
 docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ls -al"
 
-docker exec docker_mysql_db_1 bash -c "pwd"
+docker exec docker_mysql_db_1 bash -c "chmod +x /var/dummy/sample_test.sh && ls -al"
