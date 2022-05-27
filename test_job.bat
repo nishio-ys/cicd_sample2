dbf099f4ed6031e8d317ec26d25c3b287d284640
yasuhiko-nishio <yasuhiko-nishio@mamezou.com>
2022-05-27 16:37:24 +0900

    Docker削除処理追加

--- test_job.bat
+++ test_job.bat
@@ -7,3 +7,9 @@ docker-compose ps
 docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ls -al"
 
 docker exec docker_mysql_db_1 bash -c "./var/dummy/cicd_sample2/sample_test.sh"
+
+docker stop docker_mysql_db_1
+
+docker rm docker_mysql_db_1
+
+docker rmi mysql:8.0
